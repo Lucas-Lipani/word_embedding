92208b00d49589e80735b9a563ccdc47b41b0030
Lucas-Lima <lucasphenrique@hotmail.com>
2026-04-01 15:52:28 +0200

    Teste para forçar layer como false e ver o que acontece.

--- src/word_embedding/graph_sbm.py
+++ src/word_embedding/graph_sbm.py
@@ -22,7 +22,7 @@ def sbm(
     if layered:
         state_args = {
             "ec": g.ep["layer"],
-            "layers": True,
+            "layers": False,
             "eweight": g.ep["weight"],
             "pclabel": g.vp["tipo"],
         }
